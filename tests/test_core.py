25fdf1bbeec28f57064389d8ce60b3b7ce8461b0
Kranium2002 <vidhumathur2002@gmail.com>
2023-03-02 18:16:24 +0530

    Tried: fixing CD

--- tests/test_core.py
+++ tests/test_core.py
@@ -6,7 +6,6 @@ if sys.platform == "win32" and (3, 8, 0) <= sys.version_info < (3, 9, 0):
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 client = TestClient(app)
-
 def test_root():
     response = client.get("/")
     assert response.status_code == 200
