828f8e7315dfa395c6e41fe1d1fdf207d031d767
Kranium2002 <vidhumathur2002@gmail.com>
2023-03-02 15:43:41 +0530

    Added:api

---
+++ api/main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+from pymongo import MongoClient
+import uvicorn
+
+
+app = FastAPI()
+
+# Connect to MongoDB Atlas
+client = MongoClient("mongodb+srv://mathurvidhu2002:hTPfNu8e26R4nyaB@cluster0.xjtky9j.mongodb.net/?retryWrites=true&w=majority")
+db = client["db1"]
+col = db["col"]
+
+@app.get("/")
+def find_all_users():
+    return {"server is running"}
+        
+        
+@app.get("/find")
+def find_all():
+    results = []
+    for doc in db.col.find({}, {"_id": 0}):
+        results.append(doc)
+    return results
+
+@app.put("/insert/{name1}")
+def insert_record(name1: str):
+    record_dict = dict()
+    record_dict = {"name": name1}
+    try:
+        result = db.col.insert_one(record_dict)
+        print(result.inserted_id)
+        return {"message": "success"}
+    except Exception as e:
+        print(str(e))
+        return {"message": "error"}
+    
+    
+if __name__ == "__main__":
+    uvicorn.run(app,port=8000)
